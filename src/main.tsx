63bb01f48189eb0c2ee4bb103302ef0fa1dacbbf
Rashik Shahjahan <rashikshahjahan@protonmail.com>
2025-01-02 18:12:27 -0500

    rev

--- src/main.tsx
+++ src/main.tsx
@@ -3,25 +3,11 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { Analytics } from "@vercel/analytics/react"
-import { PostHogProvider} from 'posthog-js/react'
-
-const options = {
-  api_host: import.meta.env.VITE_POSTHOG_API_HOST,
-}
-
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Analytics />
-    <PostHogProvider 
-      apiKey={import.meta.env.VITE_POSTHOG_API_KEY}
-      options={options}
-    >
-      <App />
-    </PostHogProvider> 
+    <App />
   </StrictMode>,
 )
-
-
-
 

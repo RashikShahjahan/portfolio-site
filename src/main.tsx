bf1c3ab92f1be1b0d5aff871353a0ad4af5ab357
RashikShahjahan <rashikshahjahan@protonmail.com>
2025-05-02 01:41:36 -0400

    rout

--- src/main.tsx
+++ src/main.tsx
@@ -5,16 +5,19 @@ import './nous-theme.css'
 import App from './App.tsx'
 import { Analytics } from "@vercel/analytics/react"
 import { AnalyticsProvider } from 'rashik-analytics-provider';
+import { BrowserRouter } from 'react-router-dom';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Analytics />
-    <AnalyticsProvider 
-        endpoint="https://analytics.rashik.sh/events" 
-        serviceName="portfolio"
-    >
-      <App />
-    </AnalyticsProvider>
+    <BrowserRouter>
+      <Analytics />
+      <AnalyticsProvider 
+          endpoint="https://analytics.rashik.sh/events" 
+          serviceName="portfolio"
+      >
+        <App />
+      </AnalyticsProvider>
+    </BrowserRouter>
   </StrictMode>,
 )
 

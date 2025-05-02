8efb65a58dbb1c79a980291b6aff92d6fdca48fe
RashikShahjahan <rashikshahjahan@protonmail.com>
2025-05-02 00:56:03 -0400

    analy

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,17 @@ import './index.css'
 import './nous-theme.css'
 import App from './App.tsx'
 import { Analytics } from "@vercel/analytics/react"
+import { AnalyticsProvider } from 'rashik-analytics-provider';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Analytics />
+    <AnalyticsProvider 
+        endpoint="https://analytics.rashik.sh/events" 
+        serviceName="portfolio"
+      >
     <App />
+    </AnalyticsProvider>
   </StrictMode>,
 )
 

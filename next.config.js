3ab9952993a542158df22d56fba8da8483855f07
RashikShahjahan <rashikshahjahan@protonmail.com>
2025-05-30 06:49:52 -0400

    outdur

--- next.config.js
+++ next.config.js
@@ -1,9 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
   trailingSlash: true,
   images: {
-    unoptimized: true
+    unoptimized: false
   },
   async redirects() {
     return [
